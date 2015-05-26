0b1d908a9563092acd873a3b669816ee6cad888f
Mordechai100 <brlogin@brlogin.com.br>
2015-05-26 20:59:53 -0300

    aprendendo java

---
+++ Bymordechai/src/mordechai/HelloWorld.java
@@ -0,0 +1,8 @@
+package mordechai;
+//comentários
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro projeto em JAVA");
+		System.out.println("Meu primeiro projeto em JAVA");
+	}
+}
