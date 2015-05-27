e26f42983c3ddddc39f24b620ce5a084806a1151
Mordechai100 <brlogin@brlogin.com.br>
2015-05-26 22:07:20 -0300

    Execício Aponte o lapis, Pagina 10, Usa a Cabeça Java

---
+++ Bymordechai/src/mordechai/DooBeePag10.java
@@ -0,0 +1,21 @@
+package mordechai;
+
+public class DooBeePag10 {
+	public static void main (String[] args){
+
+	int x = 1 ; 
+	{
+			while (x <   3  )  {
+		System.out.print("Doo");
+		System.out.print("Bee");
+		x = x + 1;
+	}
+	if (x ==  3 ) {
+		System.out.print("Do");
+	}
+	}
+
+	}
+	
+
+}
