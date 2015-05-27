1a55860b30eaa763c672bff3d1c6258c41437661
Mordechai100 <brlogin@brlogin.com.br>
2015-05-26 22:33:15 -0300

    Identação das linhas e seus espaços para melhor visualização

--- Bymordechai/src/mordechai/DooBeePag10.java
+++ Bymordechai/src/mordechai/DooBeePag10.java
@@ -1,16 +1,19 @@
 package mordechai;
 
 public class DooBeePag10 {
+	
 	public static void main(String[] args) {
 
 		int x = 1;
 		{
 			while (x < 3) {
+				
 				System.out.print("Doo");
 				System.out.print("Bee");
 				x = x + 1;
 			}
 			if (x == 3) {
+	
 				System.out.print("Do");
 			}
 		}
--- Bymordechai/src/mordechai/loopPag9.java
+++ Bymordechai/src/mordechai/loopPag9.java
@@ -1,19 +1,26 @@
 package mordechai;
+
 //comentários
 public class loopPag9 {
+
 	public static void main(String[] args) {
+
 		System.out.println("Meu primeiro projeto em JAVA");
 		System.out.println("Meu primeiro projeto em JAVA");
 		System.out.println("Use a Cabeça Java página 9");
-		int x = 1;
-				System.out.println("Antes do Loop");
-				while (x < 6) {
-					System.out.println("No Loop");
-					System.out.println("Valor de x é:  "+x);
-					x=x+2;
-				}
-				System.out.println("Este é o fim do Loop");
-				System.out.println("pagina 9 Use a Cabeça Java");
-				}
-	}
 
+		int x = 1;
+
+		System.out.println("Antes do Loop");
+
+		while (x < 6) {
+
+			System.out.println("No Loop");
+			System.out.println("Valor de x é:  " + x);
+
+			x = x + 2;
+		}
+		System.out.println("Este é o fim do Loop");
+		System.out.println("pagina 9 Use a Cabeça Java");
+	}
+}
